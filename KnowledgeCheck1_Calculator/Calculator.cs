8ea7e745fd484fbb9c29414e26d7c2468910dd04
Charlie Snellings <charlie.snellings@gmail.com>
2023-08-03 23:25:16 -0400

    Completed KnowledgeCheck

---
+++ KnowledgeCheck1_Calculator/Calculator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace KnowledgeCheck1_Calculator
+{
+    //Made class static so that methods could be called without instatiation
+    public static class Calculator
+    {
+        public static void Add(int first, int second)
+        {
+            Console.WriteLine($"{first} + {second} = {first + second} ");
+        }
+
+        public static void Subtract(int first, int second)
+        {
+            Console.WriteLine($"{first} - {second} = {first - second} ");
+        }
+
+        public static void Multiply(int first, int second)
+        {
+            Console.WriteLine($"{first} * {second} = {first * second} ");
+        }
+
+        public static void Divide(double first, double second)
+        {
+            Console.WriteLine($"{first} / {second} = {first / second} ");
+
+        }
+
+        //captures inputs, parses them, signals true if successful
+        public static bool CaptureValues(string operation, out int valueOne, out int valueTwo)
+        {
+            //capture inputs, pass the operation
+            Console.WriteLine($"Enter 2 integers to {operation}");
+            Console.WriteLine("Please enter your first number.");
+            string inputOne = Console.ReadLine();
+            Console.WriteLine("Please enter your second number.");
+            string inputTwo = Console.ReadLine();
+
+            //check if they parse correctly
+            bool isParsedOne = int.TryParse(inputOne, out valueOne);
+            bool isParsedTwo = int.TryParse(inputTwo, out valueTwo);
+
+            return isParsedOne && isParsedTwo;
+
+        }
+
+
+        //Method overload to handle doubles.
+        public static bool CaptureValues(string operation, out double valueOne, out double valueTwo)
+        {
+            Console.WriteLine($"Enter 2 integers to {operation}");
+            Console.WriteLine("Please enter your first number.");
+            string inputOne = Console.ReadLine();
+            Console.WriteLine("Please enter your second number.");
+            string inputTwo = Console.ReadLine();
+
+            bool isParsedOne = double.TryParse(inputOne, out valueOne);
+            bool isParsedTwo = double.TryParse(inputTwo, out valueTwo);
+
+            return isParsedOne && isParsedTwo;
+        }
+
+    }
+}
---
+++ KnowledgeCheck1_Calculator/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace KnowledgeCheck1_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello! Press 1 for addition, 2 for subtraction, 3 for multiplication, and 4 for division.");
+            Console.WriteLine(" Type exit to quit.");
+
+            var input = Console.ReadLine();
+            int valueOne;
+            int valueTwo;
+            string operation = " ";
+            
+
+            while (input.ToLower() != "exit")
+            {
+                switch (input)
+                {
+                    case "1":
+                        //Each case runs in the same format
+                        //Operation identifier plugs into console.writeline output
+                        operation = "add. ";
+
+                        //calls CaptureValues method and checks if returns true, if so runs the calculator method to perform the operation
+                        if (Calculator.CaptureValues(operation, out valueOne, out valueTwo))
+                        {
+                            Calculator.Add(valueOne, valueTwo);
+                        }
+                        else Console.WriteLine("One or more of the numbers is not an int");
+
+                        break;
+
+                    case "2":
+                        operation = "subtract. ";
+
+                        if (Calculator.CaptureValues(operation, out valueOne, out valueTwo))
+                        {
+                            Calculator.Subtract(valueOne, valueTwo);
+                        }
+                        else Console.WriteLine("One or more of the numbers is not an int");
+
+                        break;
+
+                    case "3":
+                        operation = "multiply. ";
+
+                        if (Calculator.CaptureValues(operation, out valueOne, out valueTwo))
+                        {
+                            Calculator.Multiply(valueOne, valueTwo);
+                        }
+                        else Console.WriteLine("One or more of the numbers is not an int");
+
+                        break;
+
+                    case "4":
+                        operation = "divide. ";
+
+                        if (Calculator.CaptureValues(operation, out valueOne, out valueTwo))
+                        {
+                            Calculator.Divide(valueOne, valueTwo);
+                        }
+                        else Console.WriteLine("One or more of the numbers is not a double");
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Unknown input");
+                        break;
+                }
+
+                Console.WriteLine("Press 1 for addition, 2 for subtraction, 3 for multiplication, and 4 for division.");
+                Console.WriteLine("Type exit to quit.");
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}s
